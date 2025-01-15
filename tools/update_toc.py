cd28ff667f026c0752aa11bf47d65cfe6d269bde
Maoxie <edwardyzt@gmail.com>
2025-01-16 00:24:23 +0800

    feat: add ruff configuration and new utility functions for markdown processing docs: add OpenWRT installation guide and update homeassistant documentation chore: update Python version requirements in pyproject.toml

---
+++ tools/update_toc.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+This script generates a structured representation of markdown files in the 'docs' directory.
+It builds a hierarchical structure of groups and pages, ignoring certain files and drafts.
+The structure is saved as a JSON file and a Table of Contents (TOC) is generated in markdown format.
+"""
+
+import json
+from pathlib import Path
+from urllib.parse import quote
+
+from pydantic_settings import BaseSettings
+from rich import print
+
+from .utils.structure import CJSONEncoder, GroupInfo, PageInfo
+
+ROOT = Path(__file__).resolve().parents[1]
+DOCS_ROOT = ROOT / 'docs'
+
+
+class Settings(BaseSettings):
+    indent_size: int = 2  # indent with 2 spaces
+    includes: tuple[str, ...] = ('*.md',)
+    excludes: tuple[str, ...] = ('./index.md', './toc.md')
+    structure_json_output: Path = DOCS_ROOT / 'structure.json'
+    toc_md_output: Path = DOCS_ROOT / 'toc.md'
+    # TOC title
+    toc_title: str = 'Table of Contents'
+
+
+settings = Settings()
+
+
+def main():
+    """Analyse the directory structure and generate `toc.md` and `structure.json`."""
+    assert DOCS_ROOT.exists()
+
+    if not settings.toc_md_output.exists():
+        print(f'[bold yellow]Warning:[/bold yellow] {settings.toc_md_output} does not exist. Creating a new one...')
+
+    # print(json.dumps(build_structure(root), indent=2, cls=MyJSONEncoder))
+    root_info = build_from_root(DOCS_ROOT)
+    with settings.structure_json_output.open('w+', encoding='utf8', newline='\n') as f:
+        json.dump(root_info.items, f, indent=2, cls=CJSONEncoder, ensure_ascii=False)
+
+    # file_paths = find_markdowns(root)
+    # side_bars_list = create_sidebar(file_paths)
+
+    # with (root / "_sidebar.md").open("w+", encoding="utf8") as f:
+    #     f.write("\n".join(side_bars_list))
+
+    toc_lines = make_toc_content(root_info)
+    toc_lines = _format_toc(toc_lines)
+    print('[bold]Table of Contents:[/bold]')
+    print('\n'.join(toc_lines))
+
+    lines = ['<!-- TOC -->', *toc_lines, '<!-- /TOC -->']
+    if settings.toc_md_output.exists():
+        old_lines = settings.toc_md_output.read_text(encoding='utf8').splitlines()
+        start, end = None, None
+        for i, line in enumerate(old_lines):
+            if line.strip().startswith('<!-- TOC -->'):
+                start = i
+            elif line.strip().startswith('<!-- /TOC -->'):
+                end = i
+        if start is not None and end is not None:
+            lines = old_lines[: start + 1] + toc_lines + old_lines[end:]
+    settings.toc_md_output.write_text('\n'.join(lines), encoding='utf8', newline='\n')
+    print('[bold green]Done![/bold green]')
+
+    for x in root_info.items:
+        print(x.model_dump())
+
+
+def build_from_root(root: Path) -> GroupInfo:
+    """Build a structure of the directory.
+
+    Args:
+        root: The root directory.
+    """
+    root_group = GroupInfo.build_group(folder=root, root=root, includes=settings.includes, excludes=settings.excludes)
+    root_group.text = settings.toc_title
+    return root_group
+
+
+def make_toc_content(
+    item: GroupInfo | PageInfo,
+    lines: list[str] | None = None,
+    level: int = 0,
+    min_level: int = 0,
+) -> list[str]:
+    """Generate Table of Contents (TOC) for a group recursively."""
+    if lines is None:
+        lines = []
+
+    if level >= min_level:
+        if line_content := _make_line(item):
+            indent = _get_indented_prefix(level)
+            lines.append(f'{indent} {line_content}')
+    if children := getattr(item, 'items', None):
+        for c in children:
+            make_toc_content(c, lines, level=level + 1)
+    return lines
+
+
+def _get_indented_prefix(level: int, max_header_level: int = 3) -> str:
+    if level < max_header_level:
+        # should be formatted as header
+        return '#' * (level + 1)
+    else:
+        # should be formatted as list
+        return ' ' * settings.indent_size * (level - max_header_level) + '*'
+
+
+def _make_line(item: PageInfo | GroupInfo) -> str:
+    """Make a line for TOC content."""
+    if isinstance(item, GroupInfo):
+        return item.text
+    else:
+        return f'[{item.text}]({quote(item.link)})'
+
+
+def _format_toc(lines: list[str]) -> list[str]:
+    """Format the TOC content.
+
+    Rules:
+    - Insert a blank line between headers and contents.
+    """
+    insert_blank_lines_idx = []
+    for i in range(len(lines) - 1):
+        l1, l2 = lines[i], lines[i + 1]
+        l1 = l1.strip()
+        l2 = l2.strip()
+        if (l1.startswith('#') and l2.startswith('#')) or (l1.startswith('#') and l2) or (l1 and l2.startswith('#')):
+            print(l1[:5], '\t', l2[:5])
+            insert_blank_lines_idx.append(i + 1)
+    for idx in reversed(insert_blank_lines_idx):
+        lines.insert(idx, '')
+    return lines
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools/utils/structure.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import fnmatch
+import json
+from abc import ABCMeta
+from pathlib import Path
+from typing import Self
+
+from pydantic import BaseModel as _BaseModel
+from pydantic import Field
+from pypinyin import Style, lazy_pinyin
+
+
+class BaseItem(_BaseModel, metaclass=ABCMeta):
+    text: str
+
+
+class PageInfo(BaseItem):
+    text: str
+    link: str
+
+    @classmethod
+    def build_page(
+        cls,
+        filepath: Path,
+        root: Path,
+        *,
+        includes: tuple[str, ...] = tuple(),
+        excludes: tuple[str, ...] = tuple(),
+    ) -> Self | None:
+        """Build a page_info instance from a markdown file.
+
+        Args:
+            p (Path): path to the markdown file.
+            root (Path): root directory.
+
+        Returns:
+            Optional[PageInfo]: a page_info instance or None.
+        """
+        rel_p = filepath.relative_to(root)
+        rel_p_str = f'./{rel_p.as_posix()}'
+        if not any(fnmatch.fnmatch(rel_p_str, pattern) for pattern in includes):
+            return
+        if any(fnmatch.fnmatch(rel_p_str, pattern) for pattern in excludes):
+            return
+
+        # post = frontmatter.load(p.as_posix())
+        text = filepath.stem
+        if text == 'index':
+            text = filepath.parent.name
+            link = f'/{rel_p.parent.as_posix()}/'
+        else:
+            link = f'/{rel_p.as_posix()}'
+        return cls(text=text, link=link)
+
+
+class GroupInfo(BaseItem):
+    text: str
+    items: list[PageInfo | Self] = Field(default_factory=list)
+    collapsed: bool = False
+
+    @classmethod
+    def build_group(
+        cls,
+        folder: Path,
+        root: Path,
+        *,
+        depth: int = 0,
+        includes: tuple[str, ...] = tuple(),
+        excludes: tuple[str, ...] = tuple(),
+    ) -> Self:
+        """Build a group structure from a folder path.
+
+        Args:
+            folder (Path): The folder path to build the group from
+            root (Path): The root directory path used as reference for relative paths
+            depth (int, optional): Current depth level in the folder hierarchy. Defaults to 0.
+            includes (tuple[str, ...], optional): Glob patterns for files to include. Defaults to tuple().
+            excludes (tuple[str, ...], optional): Glob patterns for files to exclude. Defaults to tuple().
+
+        Raises:
+            NotImplementedError: If the method is called on an abstract base class
+
+        Returns:
+            Self: A GroupInfo instance containing the hierarchical structure of the folder
+        """
+        collapsed = depth > 2
+        group = cls(text=folder.name, collapsed=collapsed)
+        for p in sorted(
+            folder.iterdir(),
+            key=_get_pinyin_sort_key,
+        ):
+            if p.name.startswith('.'):
+                # Skip hidden files and directories
+                continue
+            if p.is_dir():
+                sub_group = cls.build_group(p, root=root, depth=depth + 1, includes=includes, excludes=excludes)
+                if sub_group.items:
+                    if len(sub_group.items) == 1:
+                        # If the sub-group contains only one item, use it as a page
+                        page = sub_group.items[0]
+                        group.items.append(page)
+                    else:
+                        group.items.append(sub_group)
+            elif p.is_file():
+                page = PageInfo.build_page(p, root=root, includes=includes, excludes=excludes)
+                if page:
+                    group.items.append(page)
+            else:
+                raise NotImplementedError(f'Unsupported file type: {p.as_posix()}')
+        return group
+
+
+class CJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        # if isinstance(o, list | tuple | set):
+        #     return [self.default(x) for x in o]
+        if hasattr(o, 'model_dump'):
+            return o.model_dump()
+        if hasattr(o, '__dict__'):
+            return o.__dict__
+        return super().default(o)
+
+
+def _get_pinyin_sort_key(p: Path) -> str:
+    text = p.name
+    # NOTE: Sort index.md first
+    if text == 'index.md':
+        return '0'
+    # NOTE: Convert alphabets to uppercase
+    # to make them sorted before Chinese characters
+    text = text.upper()
+    return ''.join(lazy_pinyin(text, style=Style.TONE3))
